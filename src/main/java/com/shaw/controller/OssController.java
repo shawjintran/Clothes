1739f3fc8755be6788d0b8a334bce9df55454667
like_wen <2279719702@qq.com>
2022-09-20 18:26:43 +0800

    修改了OssController的url名字 添加了上传图片的代码碎片

--- src/main/java/com/shaw/controller/OssController.java
+++ src/main/java/com/shaw/controller/OssController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 @RestController
-@RequestMapping("/eduoss/fileoss")
+@RequestMapping("/oss/fileoss")
 @CrossOrigin
 @Slf4j
 public class OssController {
---
+++ src/main/resources/oss.vue
@@ -0,0 +1,23 @@
+//这是上传图片组件
+<!-- 上传图片代码碎片-->
+
+<el-upload
+  class="upload-demo"
+  action="http://localhost:8080/oss/fileoss/"
+  :on-preview="handlePreview"
+  :on-remove="handleRemove"
+  :on-success="success"
+  :file-list="fileList"
+  list-type="picture">
+  <el-button size="small" type="primary">点击上传</el-button>
+  <div slot="tip" class="el-upload__tip">只能上传jpg/png文件，且不超过5MB</div>
+</el-upload>
+
+
+
+//这是结束传参方法
+  methods: {
+    success(response,file,fileList){
+      this.clothes.url=response.data.url;
+    }
+  }
